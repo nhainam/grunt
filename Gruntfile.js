4bb3123015f8d001932c71b81d298bad2f6a7684
southseapro <southseapro@gmail.com>
2017-05-06 13:32:29 +0700

    Format code

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,33 +1,33 @@
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 
-  // Project configuration.
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    uglify: {
-      build: {
-        src: ['src/js/main.js', 'src/js/index.js'],
-        dest: 'build/js/output.min.js'
-      }
-    },
-	cssmin: {
-	  options: {
-	    mergeIntoShorthands: false,
-	    roundingPrecision: -1
-	  },
-	  target: {
-	    files: {
-	      'build/css/output.min.css': ['src/css/home.css', 'src/css/main.css']
-	    }
-	  }
-	}
-  });
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        uglify: {
+            build: {
+                src: ['src/js/main.js', 'src/js/index.js'],
+                dest: 'build/js/output.min.js'
+            }
+        },
+        cssmin: {
+            options: {
+                mergeIntoShorthands: false,
+                roundingPrecision: -1
+            },
+            target: {
+                files: {
+                    'build/css/output.min.css': ['src/css/home.css', 'src/css/main.css']
+                }
+            }
+        }
+    });
 
-  // Load the plugin that provides the "uglify" task.
-  grunt.loadNpmTasks('grunt-contrib-uglify');
+    // Load the plugin that provides the "uglify" task.
+    grunt.loadNpmTasks('grunt-contrib-uglify');
 
-  // Default task(s).
-  grunt.registerTask('default', ['uglify']);
-	
-  grunt.loadNpmTasks('grunt-contrib-cssmin');
+    // Default task(s).
+    grunt.registerTask('default', ['uglify']);
+
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
 
 };
